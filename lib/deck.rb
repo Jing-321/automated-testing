e1cb91b30f6dc60e26be3ff844515d4e48f8c2b0
Yingjing Lin <yjlin789@gmail.com>
2020-09-28 23:18:51 -0700

    complete and pass all the test for deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,16 +4,41 @@
 require_relative 'card'
 
 class Deck
+  attr_accessor :cards
 
   def initialize
+    @cards = []
+
+    [:hearts, :spades, :clubs, :diamonds].each do |suit|
+      (1..13).each do |value|
+        @cards << Card.new(value, suit)
+      end
+    end
 
   end
 
   def draw
-    # returns a card
+    if @cards.empty?
+      raise ArgumentError, 'There is no card in deck.'
+    else
+      return @cards.delete_at(rand(count))
+    end
   end
 
   def shuffle
-    # shuffles the deck
+    if @cards.empty?
+      raise ArgumentError, 'There is no card in deck.'
+    else
+      return @cards.shuffle!
+    end
+  end
+
+  def count
+    if @cards.empty?
+      raise ArgumentError, 'There is no card in deck.'
+    else
+      return @cards.length
+    end
   end
 end
+
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,9 +1,40 @@
 require_relative 'test_helper'
 
+
 describe Deck do
-  # Write tests for the Deck class.
-  # What should you test?
-    # 1.  Expected Behavior
-    # 2.  Edge cases
+  let(:deck) {Deck.new}
+
+  it "can create 52 card objects as attributes" do
+    expect(deck.cards).must_be_instance_of Array
+    expect(deck.cards.length).must_equal 52
+    expect(deck.cards.first).must_be_instance_of Card
+  end
+
+  it "all cards are unique" do
+    expect(deck.cards.uniq.length).must_equal deck.cards.length
+  end
+
+  it "draw a card" do
+    expect(deck.draw).must_be_instance_of Card
+  end
+
+  # it "shuffle" do
+  #   deck_1 = Deck.new
+  #   deck_2 = Deck.new
+  #   expect(deck_1.cards).must_equal deck_2.cards
+  #   expect(deck_1.shuffle).wont_equal deck_2.cards
+  # end
+  #
+  it "return a integer for count" do
+    expect(deck.count).must_be_instance_of Integer
+  end
+
+  it "raise an ArgumentError while deck.cards is nil" do
+    deck_1 = Deck.new
+    deck_1.cards = []
+    expect{deck_1.draw}.must_raise ArgumentError
+    expect{deck_1.shuffle}.must_raise ArgumentError
+    expect{deck_1.count}.must_raise ArgumentError
+  end
 
 end
